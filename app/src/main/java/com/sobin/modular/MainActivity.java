eeaa3fd9d7746c5d216c83be24e1fc880682c3f0
SobinYuan <yuanshuobin@qq.com>
2016-11-09 19:07:57 +0800

    修正逻辑

--- app/src/main/java/com/sobin/modular/MainActivity.java
+++ app/src/main/java/com/sobin/modular/MainActivity.java
@@ -1,9 +1,11 @@
 package com.sobin.modular;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import com.sobin.modularone.OneActivity;
 import com.sobin.modularone.OneConfig;
 import com.sobin.modularthree.ThreeConfig;
 import com.sobin.modulartwo.TwoConfig;
@@ -15,10 +17,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         Toast.makeText(this, OneConfig.SDKCHANNEL+ TwoConfig.SDKCHANNEL+ ThreeConfig.SDKCHANNEL,Toast.LENGTH_SHORT).show();
 //        Toast.makeText(this, ThreeConfig.SDKCHANNEL+"==="+ LibConfig.Libcc,Toast.LENGTH_SHORT).show();
 
-
+        startActivity(new Intent(this, OneActivity.class));
 
 
     }
